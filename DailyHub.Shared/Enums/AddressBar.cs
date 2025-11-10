4dc5e5f286aa0a86f4dcab43363385a7b166349c
REZA <rreza.aak@gmail.com>
2025-11-10 21:39:24 +0330

    Project Finish !!

---
+++ DailyHub.Shared/Enums/AddressBar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyHub.Shared.Enums
+{
+    public enum AddressBar : byte
+    {
+        Home,
+        Crypto,
+        Wheater,
+        News,
+        AiCaht
+    }
+}
